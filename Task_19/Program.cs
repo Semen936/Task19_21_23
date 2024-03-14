42408836d48016f9a7bc5cf2137555065eac7cd4
Semen <1488semen@mail.ru>
2024-03-14 19:12:07 +0300

    1!

---
+++ Task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 > нет;  12821 -> да; 23432 -> да    */
+
+Console.WriteLine ("Введите 5-ти значное число: ");
+#pragma warning disable CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+string number = Console.ReadLine();
+#pragma warning restore CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+#pragma warning disable CS8602 // Разыменование вероятной пустой ссылки.
+int len = number.Length;
+#pragma warning restore CS8602 // Разыменование вероятной пустой ссылки.
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine(number + " да");
+    }
+
+    else
+    {
+        Console.WriteLine(number + " нет");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы ввели НЕ 5-и значное число");
+}
